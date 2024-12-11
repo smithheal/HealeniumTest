abd1a31e4234fee39fb56cd059e4aca46cd257d2
Smith R <smith.r@eviden.com>
2024-12-11 13:10:40 +0530

    test

--- src/test/java/com/testHeal/TestHealenium.java
+++ src/test/java/com/testHeal/TestHealenium.java
@@ -18,12 +18,12 @@ public class TestHealenium {
         WebDriver delegate = new ChromeDriver(options);
         SelfHealingDriver driver =  SelfHealingDriver.create((WebDriver) delegate);
 
-        //driver.get("https://www.homedepot.com");
+        driver.get("https://www.homedepot.com");
 
-        driver.get("http://localhost:3000/");
+       // driver.get("http://localhost:3000/");
 
         driver.manage().window().maximize();
-
+/*
         driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys("Healenium");
         driver.findElement(By.xpath("//input[@name='lastName']")).sendKeys("Execution");
         driver.findElement(By.xpath("//input[@name='email']")).sendKeys("heal@yopmail.com");
@@ -39,17 +39,22 @@ public class TestHealenium {
 
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView()", driver.findElement(By.xpath("//*[text()='User Registration Form']")));
 
+*/
 
-
-        /* === THD Test
+       //  === THD Test
         //p[text()='Log In']
 
         Thread.sleep(1000);
-       // driver.findElement(By.xpath("//p[text()='Log In']")).click();
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView()", driver.findElement(By.xpath("(//a[text()='My Preference Center'])[2]")));
-        driver.findElement(By.xpath("(//a[text()='My PreferenceCenter'])[2]")).click();
+     //   driver.findElement(By.xpath("//p[text()='Log In']")).click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView()", driver.findElement(By.xpath("//a[@class='sui-ml-1 md:sui-underline' and text()='My Preference Center']")));
 
-*/
+        driver.findElement(By.xpath("//a[@class='sui-ml-1 md:sui-underline' and text()='My Preference Center']")).click();
+
+        driver.findElement(By.xpath("//input[@name='email-search']")).sendKeys("testheal@yopmail.com");
+
+
+
+
     }
 
 }
